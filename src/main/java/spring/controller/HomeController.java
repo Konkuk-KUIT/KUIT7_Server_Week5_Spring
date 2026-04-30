248917fdde76dc10872d3ee81a9deef4c0069fd8
hahahanjihun <lego0811@naver.com>
2026-04-30 15:51:32 +0900

    5주차 미션 끝

--- src/main/java/spring/controller/HomeController.java
+++ src/main/java/spring/controller/HomeController.java
@@ -1,4 +1,16 @@
 package spring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 }
--- src/main/java/spring/controller/MemberController.java
+++ src/main/java/spring/controller/MemberController.java
@@ -1,4 +1,43 @@
 package spring.controller;
 
+import spring.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import spring.service.MemberService;
+
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원 목록
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("members", memberService.findAll());
+        return "members";
+    }
+
+    // 회원 등록 폼
+    @GetMapping("/new-form")
+    public String form() {
+        return "new-form";
+    }
+
+    // 회원 상세
+    @GetMapping("/detail")
+    public String detail(@RequestParam Long id, Model model) {
+        model.addAttribute("member", memberService.findById(id));
+        return "detail";
+    }
+
+    // 회원 저장
+    @PostMapping("/save")
+    public String save(@RequestParam String name) {
+        memberService.save(new Member(name));
+        return "redirect:/members";
+    }
 }
