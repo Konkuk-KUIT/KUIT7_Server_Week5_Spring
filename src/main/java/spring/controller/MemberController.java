db991290e74af282739cba611b041f4252c5b132
JunSeoChoi <evan7484@gmail.com>
2026-04-30 16:01:48 +0900

    최준서 서버 5주차 실습

--- src/main/java/spring/controller/MemberController.java
+++ src/main/java/spring/controller/MemberController.java
@@ -1,4 +1,45 @@
 package spring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring.domain.Member;
+import spring.service.MemberService;
+
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestParam String name, Model model) {
+        Member member = new Member(name);
+        memberService.save(member);
+
+        model.addAttribute("members", memberService.findAll());
+        return "members";
+    }
+
+    @GetMapping
+    public String members(Model model) {
+        model.addAttribute("members", memberService.findAll());
+        return "members";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam Long id, Model model) {
+        model.addAttribute("member", memberService.findById(id));
+        return "detail";
+    }
 }
