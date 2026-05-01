d99b3391e5fe0cb211432e42ebf7a9e922cb4b4a
Nayeon07 <kimny5726@gmail.com>
2026-05-01 22:11:19 +0900

    feat: 5th mission

--- src/main/java/spring/service/MemberService.java
+++ src/main/java/spring/service/MemberService.java
@@ -1,4 +1,32 @@
 package spring.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.domain.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
 public class MemberService {
+
+    private final List<Member> members = new ArrayList<>();
+
+    public void save(Member member) {
+        members.add(member);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Member> findAll() {
+        return members;
+    }
+
+    @Transactional(readOnly = true)
+    public Member findById(Long id) {
+        return members.stream()
+                .filter(m -> m.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
 }
