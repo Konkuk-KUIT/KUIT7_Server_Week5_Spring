97ab7d9c0087789e984efcee1ed3b17ef89e0c54
ajm6238 <ajm9668@naver.com>
2026-05-02 20:07:56 +0900

    안진모 5주차 과제

--- src/main/java/spring/service/MemberService.java
+++ src/main/java/spring/service/MemberService.java
@@ -1,4 +1,36 @@
 package spring.service;
 
+import domain.Member;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
 public class MemberService {
+    private static final service.MemberService instance = new service.MemberService();
+    private final List<Member> members = new ArrayList<>();
+
+    public static service.MemberService getInstance() {
+        return instance;
+    }
+
+    @Transactional
+    public void save(Member member) {
+        members.add(member);
+    }
+
+    public List<Member> findAll() {
+        return members;
+    }
+
+    public Member findById(Long id) {
+        return members.stream()
+                .filter(m -> m.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
