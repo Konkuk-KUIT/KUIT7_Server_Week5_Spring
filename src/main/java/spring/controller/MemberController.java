98d13d68667cfd32b01d2ce5048d2fc2baba5a93
신보연 <eungehby@konkuk.ac.kr>
2026-05-02 17:55:51 +0900

    요구사항 1~4

--- src/main/java/spring/controller/MemberController.java
+++ src/main/java/spring/controller/MemberController.java
@@ -1,4 +1,52 @@
 package spring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring.domain.Member;
+import spring.service.MemberService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "members/new-form";
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestParam("name") String name,
+                       Model model) {
+
+        Member member = new Member(name);
+        memberService.save(member);
+
+        model.addAttribute("member", member);
+        return "members/detail";
+    }
+
+    @GetMapping
+    public String members(Model model) {
+        List<Member> members = memberService.findAll();
+        model.addAttribute("members", members);
+        return "members/members";
+    }
+
+    // 상세 페이지로 넘어가기
+    @GetMapping("/detail")
+    public String detail(@RequestParam("id") Long id, Model model) {
+        Member member = memberService.findById(id);
+        model.addAttribute("member", member);
+        return "members/detail";
+    }
 }
--- src/main/java/spring/service/MemberService.java
+++ src/main/java/spring/service/MemberService.java
@@ -1,4 +1,29 @@
 package spring.service;
 
+import org.springframework.stereotype.Service;
+import spring.domain.Member;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class MemberService {
+
+    private static final Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public void save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public Member findById(Long id) {
+        return store.get(id);
+    }
 }
