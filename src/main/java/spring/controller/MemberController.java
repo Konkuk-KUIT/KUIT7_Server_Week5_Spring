3cab522e16c71ee1ab860d3777e7f3904069cbb7
minsk601 <minsk601@naver.com>
2026-05-02 21:43:34 +0900

    feat: Controller 통합, Spring Bean 등록, 의존관계 자동 주입

--- src/main/java/spring/controller/MemberController.java
+++ src/main/java/spring/controller/MemberController.java
@@ -1,4 +1,43 @@
 package spring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring.domain.Member;
+import spring.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping
+    public String memberList(Model model) {
+        model.addAttribute("members", memberService.findAll());
+        return "members";
+    }
+
+    @GetMapping("/detail")
+    public String memberDetail(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("member", memberService.findById(id));
+        return "detail";
+    }
+
+    @GetMapping("/new-form")
+    public String memberNewForm(Model model) {
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    public String saveMember(@RequestParam("name") String name) {
+        memberService.save(new Member(name));
+        return "redirect:/members";
+    }
+
 }
--- src/main/java/spring/service/MemberService.java
+++ src/main/java/spring/service/MemberService.java
@@ -1,4 +1,23 @@
 package spring.service;
 
+import org.springframework.stereotype.Service;
+import spring.domain.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class MemberService {
+
+    private final List<Member> members = new ArrayList<>();
+
+    public void save(Member member) { members.add(member); }
+    public List<Member> findAll() { return members; }
+    public Member findById(Long id) {
+        return members.stream()
+                .filter(m -> m.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
