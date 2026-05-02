8d1fd784bb8e42f4267fb9106f878af0d4e9254b
minsk601 <minsk601@naver.com>
2026-05-02 21:47:55 +0900

    feat: Transaction 적용

--- src/main/java/spring/service/MemberService.java
+++ src/main/java/spring/service/MemberService.java
@@ -1,6 +1,7 @@
 package spring.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import spring.domain.Member;
 
 import java.util.ArrayList;
@@ -11,8 +12,13 @@ public class MemberService {
 
     private final List<Member> members = new ArrayList<>();
 
+    @Transactional
     public void save(Member member) { members.add(member); }
+
+    @Transactional(readOnly = true)
     public List<Member> findAll() { return members; }
+
+    @Transactional(readOnly = true)
     public Member findById(Long id) {
         return members.stream()
                 .filter(m -> m.getId().equals(id))
