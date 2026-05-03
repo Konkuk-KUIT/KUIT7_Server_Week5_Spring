c8b8cee8caba677a99006ba755563c19e7584db0
likescape <likescape3@gmail.com>
2026-05-03 23:17:55 +0900

    수정

--- src/main/java/spring/controller/MemberController.java
+++ src/main/java/spring/controller/MemberController.java
@@ -1,4 +1,44 @@
 package spring.controller;
 
+import spring.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+    private final MemberService service;
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("member", service.findById(id));
+        return "detail";
+    }
+
+    @GetMapping("/new-form")
+    public String form() {
+        return "new-form";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("members", service.findAll());
+        return "members";
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestParam("name") String name, Model model) {
+        Member member = new Member(name);
+        service.save(member);
+
+        // PR의 핵심: 저장 후 리다이렉트
+        return "redirect:/members";
+    }
 }
--- src/main/java/spring/service/MemberService.java
+++ src/main/java/spring/service/MemberService.java
@@ -1,4 +1,31 @@
 package spring.service;
 
+import spring.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class MemberService {
+    private final List<Member> members = new ArrayList<>();
+
+    public void save(Member member) {
+        members.add(member);
+    }
+
+    public List<Member> findAll() {
+        return members;
+    }
+
+    public Member findById(Long id) {
+        return members.stream()
+                .filter(m -> m.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+    }
 }
