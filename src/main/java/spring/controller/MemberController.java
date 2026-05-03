ea2e9576ac8b3e00d0644d06daa892ac40984137
Ssopaa <tpgus1231234@naver.com>
2026-05-03 10:32:04 +0900

    5주차 미션 구현

--- src/main/java/spring/controller/MemberController.java
+++ src/main/java/spring/controller/MemberController.java
@@ -1,4 +1,39 @@
 package spring.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import spring.domain.Member;
+import spring.service.MemberService;
+
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    public String save(@RequestParam String name) {
+        memberService.save(new Member(name));
+        return "redirect:/members";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("members", memberService.findAll());
+        return "members";
+    }
+
+    @GetMapping("/detail")
+    public String detail(@RequestParam Long id, Model model) {
+        model.addAttribute("member", memberService.findById(id));
+        return "detail";
+    }
 }
--- src/main/java/spring/service/MemberService.java
+++ src/main/java/spring/service/MemberService.java
@@ -1,4 +1,33 @@
 package spring.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import spring.domain.Member;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final List<Member> members = new ArrayList<>();
+
+    @Transactional
+    public void save(Member member) {
+        members.add(member);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Member> findAll() {
+        return members;
+    }
+
+    @Transactional(readOnly = true)
+    public Member findById(Long id) {
+        return members.stream()
+                .filter(m -> m.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
 }
